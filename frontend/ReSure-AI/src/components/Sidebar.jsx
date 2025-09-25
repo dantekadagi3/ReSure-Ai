7567c9baccebf15a0893cc81ae58801bc2c719b9
dantekashy <kadagidante3@gmail.com>
2025-09-25 11:45:38 +0300

    created the frontend structure

---
+++ frontend/ReSure-AI/src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+
+
+
+
+
+export default function Sidebar(){
+    {/*This is the miain div for the sidebar*/}
+    <div>
+         <div>
+            <h1>The logo</h1>
+         </div>
+
+         <div>
+            
+         </div>
+    </div>
+}
